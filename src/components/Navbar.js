4c58c06dc4b6e06781b83d5b5acd715d0126a35c
Sean Wangelin <seanwangelin@outlook.com>
2023-10-15 23:00:32 -0500

    basic header looking good

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../style/navbar.css';
 
 export default function Navbar(){
 
     return (
-        <div>
-            <Link to="/">Home</Link>
+        <div id='navbar'>
+            <div>
+                <div>Mastercare</div>
+                <div>Building Services Inc.</div>
+            </div>
+            <Link to="/" className="navLink">Home</Link>
+            <Link to="/Services" className="navLink">Services</Link>
+            <Link to="/About" className="navLink">About</Link>
+            <Link to="/Contact" className="navLink">Contact</Link>
         </div>
     )
 }
