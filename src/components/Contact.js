da03b59eea8bb64859d3b94bafc143829fffd43d
Sean Wangelin <seanwangelin@outlook.com>
2023-11-27 20:35:40 -0600

    did it and im sticking with it

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -35,36 +35,52 @@ export default function Contact({ managerArray, setManagerArray }) {
   return (
     <>
       <div id="contactContainer">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.9260393700333!2d-87.77972068853371!3d41.894447671120005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880e34aa1494abc9%3A0x990ad78d4f8eeb26!2s41%20Chicago%20Ave%2C%20Oak%20Park%2C%20IL%2060302!5e0!3m2!1sen!2sus!4v1701029188364!5m2!1sen!2sus"
-            width="600"
-            height="450"
-            style={{ border: "0" }}
-            allowfullscreen=""
-            loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
-          ></iframe>
-          <div id="contactInfoContainer">
-            <div id="contactMcare">Mastercare Building Services</div>
-            <div>
-              <div>41 Chicago Ave.</div>
-              <div>Oak Park, IL 60302</div>
-            </div>
-            <a href="tel:7083582634">708-358-2634</a>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.9260393700333!2d-87.77972068853371!3d41.894447671120005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880e34aa1494abc9%3A0x990ad78d4f8eeb26!2s41%20Chicago%20Ave%2C%20Oak%20Park%2C%20IL%2060302!5e0!3m2!1sen!2sus!4v1701029188364!5m2!1sen!2sus"
+          width="600"
+          height="450"
+          style={{ border: "0" }}
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+        <div id="contactInfoContainer">
+          <div id="contactMcare">Mastercare Building Services</div>
+          <div>
+            <div>41 Chicago Ave.</div>
+            <div>Oak Park, IL 60302</div>
           </div>
+          <a href="tel:7083582634" id='contactMainPhone'>708-358-2634</a>
+        </div>
       </div>
-      {managerArray.map((manager) => {
-        return (
-          <div id='managerContainer'>
-            <div key={manager.id} className='managerCard'>
-            <div>{manager.name}</div>
-            {manager.title?<div>{manager.title}</div>:null}
-            {manager.phone?<a href={`tel:${manager.phone}`}>{manager.phone}</a>:null}
-            {manager.email?<a href={`mailto:${manager.email}`}>{manager.email}</a>:null}
-          </div>
-          </div>
-        )
-      })}
+      <div id="managerCardContainer">
+        {managerArray.map((manager) => {
+          return (
+            <div key={manager.id} className="managerCard">
+              <div>
+                <div className="managerName">{manager.name}</div>
+                {manager.title ? (
+                  <div className="managerTitle">{manager.title}</div>
+                ) : null}
+              </div>
+              <div className="contactInfoContainer">
+                {manager.phone ? (
+                  <div className="contactInfo">
+                    <img src={phoneImage} className="emailImage" />
+                    <a href={`tel:${manager.phone}`}>{manager.phone}</a>
+                  </div>
+                ) : null}
+                {manager.email ? (
+                  <div className="contactInfo">
+                    <img src={emailImage} className="emailImage" />
+                    <a href={`mailto:${manager.email}`}>{manager.email}</a>
+                  </div>
+                ) : null}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
